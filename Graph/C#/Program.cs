f36f801e1f5f28362731d86acc3bf0a403befc05
Sean Jensen <itskolesean@gmail.com>
2021-10-14 15:27:02 +0200

    inital Commit

---
+++ Graph/C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentCustomer = Customers
+                .Create("Kim")
+                .Previous("Hans")
+                .Previous("Ole")
+                .Previous("Peter");
+
+            while (currentCustomer != null)
+            {
+                if (currentCustomer.Next != null)
+                    Console.Write(currentCustomer.Person + " -> ");
+                else
+                    Console.WriteLine(currentCustomer.Person);
+
+                currentCustomer = currentCustomer.Next;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
